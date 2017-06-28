3fe480fc06788d0ac1b94cb1c8c0a34a33ce73bb
chr1123 <chr1123@qq.com>
2017-06-28 15:24:04 +0800

    WaveView实现基本功能

--- app/src/main/java/com/chrischen/androidwaveview/MainActivity.java
+++ app/src/main/java/com/chrischen/androidwaveview/MainActivity.java
@@ -1,13 +1,50 @@
 package com.chrischen.androidwaveview;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.chrischen.waveview.WaveView;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    WaveView waveView;
+    MyHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        waveView = (WaveView) findViewById(R.id.waveView);
+
+        handler = new MyHandler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!waveView.hasOver()){
+                    Message msg =handler.obtainMessage();
+                    handler.sendMessage(msg);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
     }
+
+
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            Random random = new Random();
+            waveView.putValue(random.nextInt(100));
+        }
+    }
+
 }
--- waveview/src/main/java/com/chrischen/waveview/WaveView.java
+++ waveview/src/main/java/com/chrischen/waveview/WaveView.java
@@ -1,9 +1,17 @@
 package com.chrischen.waveview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chris on 2017/6/28.
@@ -21,4 +29,134 @@ public class WaveView extends View {
     public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+
+    private List<Integer> values;//存放数值
+    private int fullValue = 100;//相对最大值
+    private float mScale = 0;//传入值转换为有效数值需要使用的比例
+    private int maxValue = 1;//当前数组中的最大值 该值乘以scale应等于fullValue
+    private int spaceEachLine=50;//竖线之间的间隔宽度
+    private int lineWidth = 10;//竖线的宽度
+    private int maxLineCount ;
+
+    private boolean hasOver;//值记录是否已完毕
+
+    public void putValue(int value){
+        if (value>maxValue){
+            maxValue = value;
+            mScale = (float) fullValue/maxValue;
+        }
+        if (values==null){
+            values = new ArrayList<>();
+        }else {
+//            values.add(value);
+//            invalidate();
+        }
+        values.add(value);
+        invalidate();
+    }
+
+    public void setHasOver(boolean over){
+        hasOver = over;
+    }
+
+    public boolean hasOver(){
+        return hasOver;
+    }
+
+
+    private int lastX,moveX;
+    private boolean hasBeenEnd=false;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) (event.getRawX());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int x = (int) event.getRawX();
+                //到达边缘时不能向该方向继续移动
+                if (!hasBeenEnd || (moveX>0&&(lastX-x)<0 ||(moveX<0&&(lastX-x)>0)))  {
+                    moveX += (lastX-x)*0.7;
+                    lastX = x;
+                    invalidate();
+                }
+                break;
+        }
+        return true;
+    }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int yCenter = getHeight() / 2;
+        if (maxLineCount==0){
+            maxLineCount = getWidth()/(spaceEachLine+lineWidth);
+        }
+        /***************画中线*****************/
+        Paint paintCenterLine = new Paint();
+        paintCenterLine.setStrokeWidth(2);
+        paintCenterLine.setColor(Color.BLACK);
+        canvas.drawLine(0,yCenter,getWidth(),yCenter,paintCenterLine);
+        /***************画竖线*****************/
+        //判断当前数组中的数据是否超出了可画竖线最大条数
+        if(values!=null){
+            //竖线画笔
+            Paint paintLine = new Paint();
+            paintLine.setStrokeWidth(lineWidth);
+            paintLine.setAntiAlias(true);
+            paintLine.setColor(Color.GREEN);
+
+            /**找出当前第一条竖线以及偏移量*/
+            int startIndex = 0;//第一条线
+            int startOffset = 0;//第一条线的偏移
+            if (!hasOver || moveX==0){//仍在记录中或未手动滑动过
+                //线条数量超出最大数 只画后面的线
+                if(values.size()>maxLineCount){
+                    startIndex = values.size()-maxLineCount;
+                }
+            }else {//已结束录值 且x轴有过移动
+                //先得到第一条线原本应该的位置
+                if(values.size()>maxLineCount){
+                    startIndex = values.size()-maxLineCount;
+                }
+                //计算移动线条数
+                int moveLineSize = moveX/(lineWidth+spaceEachLine);
+                startOffset = moveX%(lineWidth+spaceEachLine);
+                int currentIndex = startIndex+moveLineSize;
+                if (currentIndex<0){//到达最左边
+                    startIndex = 0;
+                    startOffset = 0;
+                    hasBeenEnd = true;
+                }else if (currentIndex>=values.size()){
+                    startIndex = values.size()-1;
+                    startOffset=0;
+                    hasBeenEnd = true;
+                }else {
+                    startIndex = currentIndex;
+                    hasBeenEnd = false;
+                }
+                Log.d("XXXXXXX","move-x:"+moveX+"   moveLineSize:"+moveLineSize
+                        +"   startIndex:"+startIndex+"  startOffset:"+startOffset);
+            }
+            //画竖线
+            for (int i=startIndex;i<values.size();i++){
+                int startX = (i-startIndex)*(spaceEachLine+lineWidth)+lineWidth/2 - startOffset ;
+                int endX =  startX;
+                int lineHeight = (int) ((((float)values.get(i)*mScale)/fullValue)*getHeight());
+                int startY = (getHeight()-lineHeight)/2;
+                int endY = (getHeight()-lineHeight)/2+lineHeight;
+                canvas.drawLine(startX,startY,endX,endY,paintLine);
+                Paint pNum = new Paint();
+                pNum.setColor(Color.RED);
+               // canvas.drawText(""+i,startX,yCenter,pNum); 画出竖线index便于测试
+            }
+
+
+
+        }
+    }
+
+
 }
