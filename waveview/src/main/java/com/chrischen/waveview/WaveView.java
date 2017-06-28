2fda347335dcd1c11aa5ffee122c241ebcc8bba1
chr1123 <chr1123@qq.com>
2017-06-28 15:02:38 +0800

    create waveview

---
+++ waveview/src/main/java/com/chrischen/waveview/WaveView.java
@@ -0,0 +1,24 @@
+package com.chrischen.waveview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by chris on 2017/6/28.
+ */
+
+public class WaveView extends View {
+    public WaveView(Context context) {
+        super(context);
+    }
+
+    public WaveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
