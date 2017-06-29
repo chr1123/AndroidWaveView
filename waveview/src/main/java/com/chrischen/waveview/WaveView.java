27f23011f8e514c1235a6099fa11e24acaf6e9a5
chr1123 <chr1123@qq.com>
2017-06-29 16:29:37 +0800

    添加了一些样式的自定义，如波形类型、线条宽度、颜色、间距

--- waveview/src/main/java/com/chrischen/waveview/WaveView.java
+++ waveview/src/main/java/com/chrischen/waveview/WaveView.java
@@ -1,10 +1,12 @@
 package com.chrischen.waveview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -24,22 +26,56 @@ public class WaveView extends View {
 
     public WaveView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
+
+    private Integer waveType;//波形展示类型
+    private int centerLineColor = Color.BLACK;
+    private int centerLineWidth = 1;
+    private int lineColor = Color.GREEN;
+    private int lineWidth = 10;//竖线的宽度
+    private int lineSpace = 30;//竖线之间的间隔宽度
+
+    public static final int WVTYPE_CENTER_LINE = 0;//竖线从中间开始 向上向下长度相同
+    public static final int WVTYPE_SINGLE = 1;//竖线从底部开始向上计算
 
 
     private List<Integer> values;//存放数值
     private int fullValue = 100;//相对最大值
     private float mScale = 0;//传入值转换为有效数值需要使用的比例
     private int maxValue = 1;//当前数组中的最大值 该值乘以scale应等于fullValue
-    private int spaceEachLine=50;//竖线之间的间隔宽度
-    private int lineWidth = 10;//竖线的宽度
     private int maxLineCount ;
-
     private boolean hasOver;//值记录是否已完毕
+
+    Paint paintCenterLine,paintLine;
+
+
+
+    private void init(AttributeSet attrs){
+        final TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.WaveView);
+        waveType = typedArray.getInt(R.styleable.WaveView_wvType, 0);
+        centerLineColor = typedArray.getColor(R.styleable.WaveView_wvCenterLineColor,Color.BLACK);
+        centerLineWidth = typedArray.getDimensionPixelSize(R.styleable.WaveView_wvCenterLineWidth,1);
+        lineColor = typedArray.getColor(R.styleable.WaveView_wvLineColor,Color.GREEN);
+        lineWidth = typedArray.getDimensionPixelSize(R.styleable.WaveView_wvLineWidth,10);
+        lineSpace = typedArray.getDimensionPixelSize(R.styleable.WaveView_wvLineSpace,30);
+
+        paintCenterLine = new Paint();
+        paintCenterLine.setStrokeWidth(centerLineWidth);
+        paintCenterLine.setColor(centerLineColor);
+
+        paintLine = new Paint();
+        paintLine.setStrokeWidth(lineWidth);
+        paintLine.setAntiAlias(true);
+        paintLine.setColor(lineColor);
+    }
+
+
 
     public void putValue(int value){
         if (value>maxValue){
@@ -92,22 +128,15 @@ public class WaveView extends View {
     protected void onDraw(Canvas canvas) {
         int yCenter = getHeight() / 2;
         if (maxLineCount==0){
-            maxLineCount = getWidth()/(spaceEachLine+lineWidth);
+            maxLineCount = getWidth()/(lineSpace+lineWidth);
         }
-        /***************画中线*****************/
-        Paint paintCenterLine = new Paint();
-        paintCenterLine.setStrokeWidth(2);
-        paintCenterLine.setColor(Color.BLACK);
-        canvas.drawLine(0,yCenter,getWidth(),yCenter,paintCenterLine);
+        if (waveType==WVTYPE_CENTER_LINE){
+            /***************画中线*****************/
+            canvas.drawLine(0,yCenter,getWidth(),yCenter,paintCenterLine);
+        }
         /***************画竖线*****************/
         //判断当前数组中的数据是否超出了可画竖线最大条数
         if(values!=null){
-            //竖线画笔
-            Paint paintLine = new Paint();
-            paintLine.setStrokeWidth(lineWidth);
-            paintLine.setAntiAlias(true);
-            paintLine.setColor(Color.GREEN);
-
             /**找出当前第一条竖线以及偏移量*/
             int startIndex = 0;//第一条线
             int startOffset = 0;//第一条线的偏移
@@ -122,8 +151,8 @@ public class WaveView extends View {
                     startIndex = values.size()-maxLineCount;
                 }
                 //计算移动线条数
-                int moveLineSize = moveX/(lineWidth+spaceEachLine);
-                startOffset = moveX%(lineWidth+spaceEachLine);
+                int moveLineSize = moveX/(lineWidth+lineSpace);
+                startOffset = moveX%(lineWidth+lineSpace);
                 int currentIndex = startIndex+moveLineSize;
                 if (currentIndex<0){//到达最左边
                     startIndex = 0;
@@ -142,14 +171,28 @@ public class WaveView extends View {
             }
             //画竖线
             for (int i=startIndex;i<values.size();i++){
-                int startX = (i-startIndex)*(spaceEachLine+lineWidth)+lineWidth/2 - startOffset ;
-                int endX =  startX;
+                int startX =0;
+                int endX =0;
+                int startY =0;
+                int endY =0;
                 int lineHeight = (int) ((((float)values.get(i)*mScale)/fullValue)*getHeight());
-                int startY = (getHeight()-lineHeight)/2;
-                int endY = (getHeight()-lineHeight)/2+lineHeight;
+                switch (waveType){
+                    case WVTYPE_CENTER_LINE:
+                        startX = (i-startIndex)*(lineSpace+lineWidth)+lineWidth/2 - startOffset ;
+                        endX =  startX;
+                        startY = (getHeight()-lineHeight)/2;
+                        endY = (getHeight()-lineHeight)/2+lineHeight;
+                        break;
+                    case WVTYPE_SINGLE:
+                        startX = (i-startIndex)*(lineSpace+lineWidth)+lineWidth/2 - startOffset ;
+                        endX =  startX;
+                        startY = getHeight()-lineHeight;
+                        endY = getHeight();
+                        break;
+                }
                 canvas.drawLine(startX,startY,endX,endY,paintLine);
-                Paint pNum = new Paint();
-                pNum.setColor(Color.RED);
+               // Paint pNum = new Paint();
+               // pNum.setColor(Color.RED);
                // canvas.drawText(""+i,startX,yCenter,pNum); 画出竖线index便于测试
             }
 
